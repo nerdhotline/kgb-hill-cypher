aa77eac9a413ff96317f26dbf8f041c6ccb39518
nerdhotline <kenny.g.blake@gmail.com>
2026-02-13 11:17:41 -0500

    More reformatting

--- MTX.py
+++ MTX.py
@@ -6,22 +6,22 @@ from utils import convert
 
 # isInvertible --> Checks if matrix is both square and has a non-zero determinant
 def isInvertible(mtx:np.matrix):
-	isSquare = mtx.shape[0] == mtx.shape[1]
-	isDetZero = np.isclose(np.linalg.det(mtx), 0)
-	return isSquare and not isDetZero 
+  isSquare = mtx.shape[0] == mtx.shape[1]
+  isDetZero = np.isclose(np.linalg.det(mtx), 0)
+  return isSquare and not isDetZero 
 
 # invertMtx --> Calculates a^(-1) (mod 26) and multiplies by the adjoint matrix
 def invertMtx(mtx:np.matrix):
-	adj = np.linalg.det(mtx) * np.linalg.inv(mtx)
-	inverse = pow(int(np.linalg.det(mtx)), -1, 26)
-	adj = inverse * adj
-	return adj % 26
-	
+  adj = np.linalg.det(mtx) * np.linalg.inv(mtx)
+  inverse = pow(int(np.linalg.det(mtx)), -1, 26)
+  adj = inverse * adj
+  return adj % 26
+  
 def transform(text: str, mtx: np.matrix, isEncode:bool = True):
-	if (isInvertible(mtx)):
-		# if we are decoding, flip the matrix
-		mtx = mtx if isEncode else invertMtx(mtx)
-		vec = convert(text)
-		product = mtx @ vec
-		norm = product % 26
-		return convert(norm)
+  if (isInvertible(mtx)):
+    # if we are decoding, flip the matrix
+    mtx = mtx if isEncode else invertMtx(mtx)
+    vec = convert(text)
+    product = mtx @ vec
+    norm = product % 26
+    return convert(norm)
--- main.py
+++ main.py
@@ -1,35 +1,29 @@
 import numpy as np 
-from utils import cleanText
+from utils import cleanText, tokenize
 from MTX import transform
 
 # ----------------------------------------------------------------------------------------------------------------
 # HILL CYPHER FUNCTIONS
 
 def hillEncode(text:str, blockNum: int, key):	
-	cleaned, tokens, encodedTokens = '', [], []
+  cleaned, tokens, encodedTokens = '', [], []
 
-	# Clean text, tokenized based on blockNum
-	cleaned = cleanText(text)
-	tokens = [cleaned[i:i+blockNum] for i in range(0, len(text), blockNum)]
-	tokens = [string for string in tokens if string]
-	lastTokenDiff = blockNum - tokens[-1] 
+  # Clean text, tokenized based on blockNum
+  cleaned = cleanText(text)
+  tokens = tokenize(cleaned, len(text), blockNum)
+  
+  # Add an entire block of filler characters (optional)
+  # tokens.append('x' * blockNum)
 
-	# Normalize last token, make its length match all other tokens
-	if (lastTokenDiff != 0):
-		tokens[-1] += ('x' * lastTokenDiff)
-	
-	# Add an entire block of filler characters (optional)
-	# tokens.append('x' * blockNum)
-
-	encodedTokens = [transform(token, key) for token in tokens]
-	return ''.join(encodedTokens)
+  encodedTokens = [transform(token, key) for token in tokens]
+  return ''.join(encodedTokens)
 
 
 def hillDecode(text:str, blockNum: int, key):
-	tokens, decodedTokens = [], []
-	tokens = [text[i:i+blockNum] for i in range(0, len(text), blockNum)]
-	tokens = [string for string in tokens if string]
-	
-	decodedTokens = [transform(token, key, False) for token in tokens]
-	return ''.join(decodedTokens)
+  tokens, decodedTokens = [], []
+  tokens = [text[i:i+blockNum] for i in range(0, len(text), blockNum)]
+  tokens = [string for string in tokens if string]
+  
+  decodedTokens = [transform(token, key, False) for token in tokens]
+  return ''.join(decodedTokens)
 
